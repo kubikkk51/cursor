961b9993189c7a64ea84ec7da958597244a88f09
kubikkk51 <kubarev5482@gmail.com>
2021-09-30 13:53:03 +0300

    test

--- hw1/script.js
+++ hw1/script.js
@@ -1,49 +1,42 @@
+const eggsPrice = 15.678;
+console.log ( "Яйца: " +  eggsPrice + " грн." );
 
-  const eggs = 15.678;
-  console.log("Яйца: " +  eggs +" грн.");
+const breadPrice = 123.965;
+console.log ("Хлеб: " + breadPrice + " грн.");
 
-  const bread = 123.965;
-  console.log("Хлеб: " + bread +" грн.")
-  
-  const potato = 90.2345; 
-  console.log ("Картошка: "+ potato +" грн.");
+const potatoPrice = 90.2345; 
+console.log( "Картошка: " + potatoPrice + " грн." );
 
+const maxNumber = Math.max( eggsPrice, breadPrice, potatoPrice );
+console.log( "Максимальное число: "+ maxNumber );
 
-  const maxNumber = Math.max( eggs, bread, potato);
-  console.log ("Максимальное число: "+ maxNumber);
+const minNumber = Math.min ( eggsPrice, breadPrice, potatoPrice );
+console.log( "Минимальное число: " + minNumber );
 
-  const minNumber = Math.min( eggs, bread, potato);
-  console.log("Минимальное число: "+minNumber);
+const sumOfProducts = ( eggsPrice + breadPrice + potatoPrice );
+console.log( "Стоимость всех товаров: " + sumOfProducts + " грн." );
 
+const roundingDown = ( Math.floor(eggsPrice) + Math.floor(breadPrice) + Math.floor(potatoPrice) ); 
+console.log("Округленная стоимость товаров: " + roundingDown + " грн.");
 
-  const together = (eggs + bread + potato);
-  console.log("Стоимость всех товаров: "+together+" грн.");
+const roundingToHundreds = ( Math.round(sumOfProducts/100) ) * 100;
+console.log( "Округленная стоимость товаров до сотен: " + roundingToHundreds + " грн." );
 
-  
-  const roundingDown = (Math.floor( eggs ) + Math.floor(bread) + Math.floor(potato)); 
-  console.log("Округленная стоимость товаров: " + roundingDown+ " грн.");
+const parityСheck = roundingDown % 2 === 0;
+console.log( "Четное? " +parityСheck );
 
+const totalAmountOfMoney = 500;
+const remainder = totalAmountOfMoney - sumOfProducts;
+console.log ( "Сдача с 500 грн: " + remainder + " грн." );
 
-  const roundingToHundreds = (Math.round(together/100))*100;
-  console.log("Округленная стоимость товаров до сотен: " +roundingToHundreds + " грн.");
- 
-  
-  const parityСheck = roundingDown % 2 === 0;
-  console.log("Четное? " +parityСheck);
+const averagePrice = (sumOfProducts/3).toFixed(2);
+console.log ("Округленная до второго знака средняя цена: " + averagePrice + " грн.");
 
+const discount = Math.round(Math.random ()*100);
+console.log ( "Скидка " + discount );
 
-  const remainder = 500 - together;
-  console.log ("Сдача с 500 грн: "+ remainder + " грн.");
-
-
-  const averagePrice = (together/3).toFixed(2);
-  console.log ("Округленная до второго знака средняя цена: " + averagePrice + " грн.");
-
-    const discount = Math.round(Math.random ()*100);
-    console.log ("Скидка " + discount );
-    
-    const profit =   ((together/2)-(discount*together/100)).toFixed(2);
-    console.log ( "чистая прибіль "+ profit + " грн.");
+const profit =   ( (sumOfProducts/2) - (discount*sumOfProducts/100) ).toFixed(2);
+console.log ( "чистая прибіль " + profit + " грн." );
 
 
 
