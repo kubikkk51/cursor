4a9d84e66798a8863995d4cdc3395cd5dd53f5f8
kubikkk51 <kubarev5482@gmail.com>
2021-11-04 22:30:11 +0200

    test

---
+++ hw11/main.js
@@ -0,0 +1,26 @@
+//Створіть функцію, яка повертає проміс getRandomChinese(length). Функція працює таким чином:
+//
+//Запускається цикл(підказка: можна використовувати while) length раз, кожен прохід циклу дивимось на останні 5 цифр результату виклику методу Date.now() Наприклад отримали const sign = 16086;.
+//Конвертуємо отриману цифу в рядок String.fromCharCode(sign); та чекаємо 50 ms
+//Функція gerRandomChinese(length) повинна повернути рядок довжиною (length) з китайськими ієрогліфами. Час роботи проміса має складати length * 50ms.
+//(Якщо викликати getRandomChinese(4), отримаємо результат "촛궻簽紙" за 200ms
+function getRandomChinese(length) {
+    
+     return new Promise((resolve) => {
+         let string = "";
+         let i = 0;
+         while ( i < length ){
+             i++ 
+             setTimeout ( () => {
+                 let sign = Date.now().toString().slice(-5);
+                 string += String.fromCharCode(sign);
+                 if (string.length === length) {
+                    resolve(string);
+                }
+             }, length*50 );
+         } 
+     })  
+}
+getRandomChinese(4).then( result => console.log(result) );
+
+
